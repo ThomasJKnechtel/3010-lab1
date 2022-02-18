3d90c4707e941fc7926584430bf84dcd5d0c234a
ThomasJKnechtel <thomasknechtel@cmail.carleton.ca>
2022-02-18 12:54:04 -0500

    feet: update colour of text displayed

--- lab1/showsensordata.py
+++ lab1/showsensordata.py
@@ -6,6 +6,7 @@ sense.clear()
 pressure = sense.get_pressure()
 temp = sense.get_temperature()
 humidity= sense.get_humidity()
-sense.show_message('pressure:'+str(pressure))
+sense.show_message('pressure:'+str(pressure), text_colour=(0,0,255))
+
 sense.show_message('temperature:'+str(temp))
 sense.show_message('humidity:'+str(humidity))
